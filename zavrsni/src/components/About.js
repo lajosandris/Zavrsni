08c5cc1bf0ffe0a9793841da180103651d1de15a
lajosandris <lakanalas@gmail.com>
2023-05-24 18:55:06 +0200

    GPP

--- zavrsni/src/components/About.js
+++ zavrsni/src/components/About.js
@@ -1,5 +1,5 @@
 const About = () =>{
-    return <div><h1>About Page</h1>
+    return <div><h1>Pomoć</h1>
     </div>
 }
 export default About;
--- zavrsni/src/components/Header.js
+++ zavrsni/src/components/Header.js
@@ -6,14 +6,14 @@ const Header = (props) =>{
         {/* Prazan react fragment, služi istu svrhu kao i div, ali manje opterećuje DOM */}
         {isLoggedIn && 
         <div className="App-header">
-        <button className='sidebar-btn' onClick={props.toggleSidebar}>Sidebar</button>
-        <h1>{props.username}'s Chat Room</h1>
-        <button onClick={props.handleLogout}>Logout</button>
+        <button className='sidebar-btn' onClick={props.toggleSidebar}>Izbornik</button>
+        <h1>Pozdrav, kako vam možemo pomoći?</h1>
+        <button onClick={props.handleLogout}>Odjavi se</button>
       </div>}
 
       {!isLoggedIn &&  <div className="App-header">
         <div></div>
-        <h1>Chat Application</h1>
+        <h1>GPP Pričalica</h1>
         {/* Prazan div služi za flex pozicioniranje elemenata */}
         <div></div>
       </div>}
